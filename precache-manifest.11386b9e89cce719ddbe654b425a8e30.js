a026d0c68cbbd8a16811defa948c32bd9c7d52ed
gaev-art <forsakensr@gmail.com>
2020-09-04 17:50:38 +0300

    Updates

---
+++ precache-manifest.11386b9e89cce719ddbe654b425a8e30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b38c6be1156d5c51d38d1b9ba9812dc",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "411c7b032f567ba3b188",
+    "url": "/My-Portfolio/static/css/main.f371d071.chunk.css"
+  },
+  {
+    "revision": "77e8cef5a12a87a2c35f",
+    "url": "/My-Portfolio/static/js/2.4404d83c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Portfolio/static/js/2.4404d83c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "411c7b032f567ba3b188",
+    "url": "/My-Portfolio/static/js/main.b09accb2.chunk.js"
+  },
+  {
+    "revision": "819fb460b83bb9c6f07e",
+    "url": "/My-Portfolio/static/js/runtime-main.a442750f.js"
+  },
+  {
+    "revision": "996eb1fe1bfc3d8985d9a232b8a47df6",
+    "url": "/My-Portfolio/static/media/bg-image-28.996eb1fe.jpg"
+  },
+  {
+    "revision": "ce4290b7ce29c450ecd6f9c80f19cf4b",
+    "url": "/My-Portfolio/static/media/photo.ce4290b7.JPG"
+  }
+]);
